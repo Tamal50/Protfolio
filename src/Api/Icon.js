3f8a8c6e2801d5c82d3e9a5e3d0b319370c56a81
Tamal <tamal7971@gmail.com>
2021-08-11 03:58:29 +0600

    Project Updating

--- src/Api/Icon.js
+++ src/Api/Icon.js
@@ -5,8 +5,8 @@ import Linkdin from '../image/icon/linkedin.svg'
 
 
 const IconList = [
-    {name: 'Github', icon: Github, link: 'https://github.com/'},
-    {name: 'Linkdin', icon: Linkdin, link: 'https://facebook.com'},
-    {name: 'Facebook', icon: Facebook, link: 'https://facebook.com'}
+    {name: 'Github', icon: Github, link: 'https://github.com/Tamal50'},
+    {name: 'Linkdin', icon: Linkdin, link: 'https://www.linkedin.com/in/tamaldatta'},
+    {name: 'Facebook', icon: Facebook, link: 'https://www.facebook.com/mr.idiot.tamal'}
 ]
 export default IconList
